5a3d06c6c5a531950ca2a1cb43bf65f904fb4070
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-05-11 17:47:38 +0530

    insert at last function created and working

--- doubleLinkedList.js
+++ doubleLinkedList.js
@@ -20,11 +20,28 @@ class DoubleLinkedList{
 
         if(this.head == null){
             this.head = newNode;
+            this.tail = newNode;
             return;
         }
         newNode.next = this.head;
         this.head.prev = newNode;
         this.head = newNode;
+    }
+
+
+    insertAtLast(data){
+        const newNode = new DoubleNode(data);
+
+        if(this.head == null){
+            this.head = newNode;
+            this.tail = newNode;
+            return;
+        }
+
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+
     }
 
 
@@ -43,6 +60,21 @@ class DoubleLinkedList{
 
         console.log(str + "Null");
     }
+    printReverseList(){
+        if(this.head == null){
+            console.log("List is empty");
+        }
+
+        let currentNode = this.tail;
+        let str = "";
+
+        while(currentNode != null){
+            str += currentNode.data + " <=> ";
+            currentNode = currentNode.prev;
+        }
+
+        console.log(str + "Null");
+    }
 }
 
 const doubleLinkedList = new DoubleLinkedList();
@@ -50,7 +82,7 @@ const doubleLinkedList = new DoubleLinkedList();
 
 doubleLinkedList.insertAtFirst(12);
 doubleLinkedList.insertAtFirst(13);
-// doubleLinkedList.insertAtFirst(12);
-
+doubleLinkedList.insertAtLast(15);
 
 doubleLinkedList.printDoubleList();
+doubleLinkedList.printReverseList();
